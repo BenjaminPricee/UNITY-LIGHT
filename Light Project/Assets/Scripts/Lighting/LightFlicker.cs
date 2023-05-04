6a963cf3ef799a7422baaa60d50bb60ac349afec
unknown <c00260297@itcarlow.ie>
2023-05-04 18:20:37 +0100

    Light Flicker Script

--- Light Project/Assets/Scripts/Lighting/LightFlicker.cs
+++ Light Project/Assets/Scripts/Lighting/LightFlicker.cs
@@ -1,9 +1,52 @@
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
+using System.Collections.Generic;
 
 public class LightFlicker : MonoBehaviour
 {
-    public Light light;
+    public new Light light;
 
+    private bool flick;
+    private int flickDelay;
+    
+    private int setIntesity;
+    private float minIntensity;
+    private float maxIntensity;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        flick = false;
+        flickDelay = 0;
+        setIntesity = 1;
+        minIntensity = 0f;
+        maxIntensity = 0.75f;
+}
+
+    void Update()
+    {
+        if (flick)
+        {
+            if(flickDelay > 0)
+            {
+                flickDelay -= 1;
+            }
+            else
+            {
+                flickDelay = 0;
+                light.intensity = setIntesity;
+                flick = false;
+            }
+        }
+        else
+        {
+            int chance = Random.Range(0, 75);
+            if (chance == 1)
+            {
+                light.intensity = Random.Range(minIntensity, maxIntensity);
+                flickDelay = Random.Range(0, 50);
+                flick = true;
+            }
+        }
+    }
 
 }
