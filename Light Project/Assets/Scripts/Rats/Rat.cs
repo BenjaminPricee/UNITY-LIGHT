e7c46e5ba70441118f0c8ce85f3d5d5f593d7c3e
unknown <c00260297@itcarlow.ie>
2023-05-18 11:34:41 +0100

    Rat Attempt One

--- Light Project/Assets/Scripts/Rats/Rat.cs
+++ Light Project/Assets/Scripts/Rats/Rat.cs
@@ -8,6 +8,36 @@ public class Rat : MonoBehaviour
     private void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-        nav.SetDestination(new Vector3(0, 0, 0));
+        nav.destination = NewDestination();
+    }
+
+    private void Update()
+    {
+        if(nav.remainingDistance >1)
+        {
+            nav.destination = NewDestination();
+        }
+
+    }
+
+    private Vector3 NewDestination()
+    {
+        Vector3 newDest = new Vector3();
+
+        Vector3 randomDir = Random.onUnitSphere;
+        randomDir.y = 0;
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,randomDir,out hit))
+        {
+            newDest = hit.point;
+            newDest.y = .01f;
+        }
+        else
+        {
+            newDest = NewDestination();
+        }
+
+        return newDest;
     }
 }
